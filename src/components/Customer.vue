2b17e029b437d9bb0d09415f9d83fb5b0742d72b
Rick Berger <rbmailer-contact@yahoo.com>
2017-10-12 18:46:05 -0600

    Worked over the current customer table css

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="mdl-card__supporting-text">
         <table>
-          <tr><th>Account # (pin):</th><td>{{customerID}}</td></tr>
+          <tr><th class="cust-th" >Account # (pin):</th><td class="cust-td">{{customerID}}</td></tr>
           <tr><td colspan="2"><hr/></td></tr>
           <tr><th>Checking:</th><td>${{customerAcctInfo.balances.checking}}</td></tr>
           <tr><th>Savings:</th><td>${{customerAcctInfo.balances.savings}}</td></tr>
@@ -30,12 +30,15 @@ export default {
 }
 </script>
 <style scoped>
+  .cust-td,.cust-th { font-weight:bold; font-size: 1.3rem;}
+  .cust-td { color: crimson; }
+  .cust-td::before, .cust-td::after { content: '--'; }
   .demo-card-wide {border: thin solid #ccc; width:300px; margin-left:auto; margin-right:auto;
                    margin-top:4rem;  }
   .mdl-card__title { background-color: cyan;}
-  .mdl-card__title-text { font-size: 1.5rem; font-weight:bold; margin-left:auto; margin-right:auto; }
+  .mdl-card__title-text { color:#777; font-size: 1.5rem; font-weight:bold; margin-left:auto; margin-right:auto; }
   .title-wrapper { text-align:center!important; }
   .mdl-card__supporting-text table { width:100%; }
-  th {text-align:left; }
-  td {text-align:right; }
+  th {text-align:left; font-size:1.2rem; }
+  td {text-align:right; font-size:1.2rem; }
 </style>
